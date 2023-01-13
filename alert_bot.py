3251c23d82242e70889be71dd8b0a1ebeeda93db
Long Nguyen <nguyenlong31415@gmail.com>
2023-01-13 23:15:36 +0100

    good to go

--- alert_bot.py
+++ alert_bot.py
@@ -1,8 +1,9 @@
 from discord.ext import tasks
 from products import Product,get_latest
-from channel_client import ChannelClient
+from channel_client import ChannelClient, PriceRule
 import discord
 import json
+import time
 
 
 # Load credentials
@@ -17,41 +18,88 @@ class AlertBot:
         intents = discord.Intents.default()
         intents.message_content = True
         self.client = discord.Client(intents = intents)
-        self.channel_clients:list[ChannelClient] = []
-    
-    def get_channels(self) -> list[discord.channel.TextChannel]:
-        return list(map(lambda x: x.channel,self.channel_clients))
+        self.channel_clients:dict[str,ChannelClient] = {}
 
-    async def send_message(self,message:discord.message.Message, reponse:str):
+    async def send_message(self,channel:discord.channel.TextChannel, reponse:str):
         try:
-            await message.channel.send(reponse)
+            await channel.send(reponse)
         except Exception as e:
             print(e)
     
     async def handle_bot_message(self,message:discord.message.Message):
-        cmd = str(message.content.lower()).split()
+        user_channel= message.channel
+        cmd = str(message.content).split()
+        while len(cmd) < 7:
+            cmd.append('#')
         
+        # starts bot in channel 
         if cmd[1] == "start":
-            if message.channel not in self.get_channels():
-                new_client = ChannelClient(message.channel)
-                self.channel_clients.append(new_client)
-                print(f"{message.channel} add to client list.")
+            if user_channel.id not in self.channel_clients:
+                new_client = ChannelClient(user_channel)
+                self.channel_clients[user_channel.id] = new_client
+                print(f"{user_channel} add to client list.")
+                await self.send_message(user_channel,"BOT START\n*There are no rules for this bot yet.*")
         
+        if user_channel.id not in self.channel_clients:
+            return
+
+        # since here, bot must be started in channel
+        channel_client = self.channel_clients[user_channel.id]
         if cmd[1] == "stop":
-            for client in self.channel_clients:
-                if client.channel == message.channel:
-                    client.is_active = False
-                    print(f"{client.channel} is inactive now.")
+            self.channel_clients.pop(user_channel.id)
+            print(f"{user_channel} deleted from client list.")
+        
+        if cmd[1] == "rules":
+            msg = "The price rules are:\n"
+            for i,v in enumerate(channel_client.price_rules,start=1):
+                role,rule = v
+                msg += f"{i}. {rule} Role: {role}\n"
+            await self.send_message(user_channel,msg)
+        
+        if cmd[1] == "buzzwords":
+            msg = "The buzzwords are:\n"
+            for i,v in enumerate(channel_client.buzzwords,start=1):
+                role,word = v
+                msg += f"{i}. {word} Role: {role}\n"
+            await self.send_message(user_channel,msg)
+        
+        if cmd[1] == "add" and cmd[2] == "rule":
+            try:
+                min_price, max_price = float(cmd[3]), float(cmd[4])
+                min_disc = int(cmd[5])
+                role = cmd[6]
+                channel_client.add_rule(PriceRule(min_price,max_price,min_disc),role)
+                print(f"New rule added. ({user_channel})")
+            except:
+                await self.send_message(user_channel,"*Invalid format*\n"+
+                "bot add rule <min price> <max price> <min discount> <role id>")
+        
+        if cmd[1] == "add" and cmd[2] == "buzzword":
+            word = cmd[3]
+            role = cmd[4]
+            channel_client.add_buzzword(word,role)
+            print(f"New buzzword added. ({user_channel})")
     
 
     def run(self):
 
         @tasks.loop(seconds=10)
-        async def alert_loop():pass
+        async def alert_loop():
+            latest = get_latest()
+            for prod in latest:
+                for cc in self.channel_clients.values():
+                    valid,role = cc.product_valid(prod)
+                    if not valid: continue
+                    msg = f"{role}\n{prod}"
+                    await self.send_message(cc.channel,msg)
+                    print(f"Sent {prod.name[:20]} to {cc.channel}")
+                    cc.sended_urls.append(prod.url)
+                time.sleep(1)
         
         @self.client.event
         async def on_ready():
             print(f"{self.client.user} is running.")
+            alert_loop.start()
         
         @self.client.event
         async def on_message(message: discord.message.Message):
@@ -60,7 +108,7 @@ class AlertBot:
 
             username = str(message.author)
             user_message = str(message.content)
-            channel = str(message.channel)
+            channel = str(message.channel.id)
             print(f"{username} said: '{user_message}' ({channel})")
 
             if user_message.lower().startswith("bot "):
--- channel_client.py
+++ channel_client.py
@@ -1,25 +1,49 @@
 import discord
-import products
+from products import *
+
+class PriceRule:
+    def __init__(self,min_price = 0, max_price = 0, min_disc = 0):
+        self.min_price = min_price
+        self.max_price = max_price
+        self.min_disc = min_disc
+    
+    def __repr__(self) -> str:
+        min_price_str = float_to_price(self.min_price)
+        max_price_str = float_to_price(self.max_price)
+
+        return f"{min_price_str} - {max_price_str} and {self.min_disc}%"
+        
 
 class ChannelClient:
     def __init__(self,channel:discord.channel.TextChannel):
         self.channel : discord.channel.TextChannel = channel
-        self.is_active = True
-        self.price_rules = []
-        self.buzzwords  = []
+        self.price_rules : list[tuple[str,PriceRule]] = []  # (role,PriceRule)
+        self.buzzwords : list[tuple[str,str]] = []  # (role,str)
         self.sended_urls : list[str] = []
     
-    def add_rule(self,rule):
-        self.price_rules.append(rule)
+    def add_rule(self, rule:PriceRule, role:str):
+        self.price_rules.append((role,rule))
     
-    def remove_rule(self,rule):
-        if rule in self.price_rules:
-            self.price_rules.remove(rule)
+    def add_buzzword(self,buzzword:str, role:str):
+        self.buzzwords.append((role,buzzword))
     
-    def add_buzzword(self,buzzword):
-        self.buzzwords.append(buzzword)
-    
-    def remove_buzzword(self,buzzword):
-        if buzzword in self.buzzwords:
-            self.buzzwords.remove(buzzword)
-    
+    # returns string of roles if valid else returns empty string
+    def product_valid(self,product:Product)->tuple[bool,str]:
+        roles = set()
+        valid = False
+
+        if product.url in self.sended_urls:
+            return False,""
+
+        for role,rule in self.price_rules:
+            price_valid = (product.old_price >= rule.min_price) and (product.old_price <= rule.max_price)
+            disc_valid = (product.discount >= rule.min_disc)
+            if price_valid and disc_valid:
+                valid |= True
+                roles.add(role)
+        for role,word in self.buzzwords:
+            if word.lower() in product.name.lower():
+                valid |= True
+                roles.add(role)
+        
+        return valid,' '.join(roles)
