c725514b9381b40da64785f4fde61663cf984c14
Long Nguyen <nguyenlong31415@gmail.com>
2023-01-14 11:38:50 +0100

    update

--- scrape_time_test.py
+++ scrape_time_test.py
@@ -2,5 +2,6 @@ import products
 from time import perf_counter
 
 perf_start = perf_counter()
-products.get_latest()
+for prod in products.get_latest():
+    print(prod.name)
 print(f"Time: {perf_counter()-perf_start}s")
